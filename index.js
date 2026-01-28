7b6bb0e0ab7125e2f4e04e02c0b4e47c9af4b6a3
ziyodullaTech <tursunboyevziyodulla30@gmail.com>
2026-01-28 20:03:00 +0500

    I fixed the problem here

--- index.js
+++ index.js
@@ -6,30 +6,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// static files (public/index.html)
+// 🔥 ENG MUHIM QATOR
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-    console.log("User ulandi:", socket.id);
-
-    // 🔑 public key relay
-    socket.on("public-key", (key) => {
-        socket.broadcast.emit("public-key", key);
-    });
-
-    // 🔐 AES key relay
-    socket.on("aes-key", (data) => {
-        socket.broadcast.emit("aes-key", data);
-    });
-
-    // 💬 encrypted message relay
-    socket.on("message", (msg) => {
-        socket.broadcast.emit("message", msg);
-    });
-
-    socket.on("disconnect", () => {
-        console.log("User uzildi:", socket.id);
-    });
+    socket.on("public-key", (key) => socket.broadcast.emit("public-key", key));
+    socket.on("aes-key", (data) => socket.broadcast.emit("aes-key", data));
+    socket.on("message", (msg) => socket.broadcast.emit("message", msg));
 });
 
 const PORT = process.env.PORT || 3000;
